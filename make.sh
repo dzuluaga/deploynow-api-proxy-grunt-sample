304514f625ce5c076116accffe12d131ad56ca49
dzuluaga <dzuluaga@apigee.com>
2016-01-12 22:40:28 -0800

    including npm install

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@ npm install
 grunt --env=test --username=$ae_username --password=$ae_password --upload-modules=true --org=$org --env=$env --debug=true --force
 
 echo "Test your APIs with the following http command"
-echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/deploynow-api-proxy-grunt-sample/forecastweather_node?w=2487956"
+echo "Check this URL to test check San Francisco weather: https://$org-$env.apigee.net/deploynow-api-proxy-grunt-sample/forecastweather_node/2487956"
